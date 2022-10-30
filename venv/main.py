89fc2692e47b24ddc4a311ad58532b24ff4461f2
Rafael Augusto <104601765+Rafah01@users.noreply.github.com>
2022-10-29 21:42:02 -0300

    Update main.py
    Comentários em cada procedimento

--- venv/main.py
+++ venv/main.py
@@ -1,15 +1,17 @@
 # Estrutura de tratamento de erro
 
 try:
+    """Funções """
 
-    #Pega a faixa salarial do cliente
+    #A parti de critérios considerados, calcula a faixa salarial do cliente
     def faixa_renda():
         print("\nSua faixa de renda será especificada com base em seu salário,\ninforme-o digitando o número abaixo na faixa salarial em que se enquadra.\n")
         faixa= int(input("\n1. menos de 1 salário.\n2. Entre 1 a 2 salários.\n3. Entre 2 a 5 salários.\n4. Mais que 5 salários.\n"))
         return faixa
 
-    #valor total  pra casa
-    def casa():
+    #Calcula o valor total  para o imóvel
+
+    def imovel():
         faixa=faixa_renda()
         valor=float(input("Diga-nos o valor da residência que deseja.\n"))
         meses = int(input("Em quantos meses poderá pagar tudo?"))
@@ -26,7 +28,8 @@ try:
 
         return total
 
-    #valor total pra veículos
+    #Calcula o valor total pra veículos
+
     def veiculo():
         faixa = faixa_renda()
         valor=float(input("Diga-nos o valor do veículo que deseja.\n"))
@@ -45,10 +48,15 @@ try:
 
         return total
 
+    #Função que calcula o total do valor com juros compostos
+
     def juros_composto(valor,meses,taxa):
         montante= valor*((1+taxa)**meses)
         total=valor+ montante
         return total
+
+
+    # Função que calcula o total do valor com juros simples
 
     def  juros_simples(valor, meses,taxa):
 
@@ -56,7 +64,7 @@ try:
         total=valor+montante
         return total
 
-    #Faz o cadastro do cliente
+    #Faz o cadastro do cliente, recebendo seus dados
 
     def cadastro():
 
@@ -70,7 +78,7 @@ try:
         return nome
 
 
-    #main
+    #Programa ´principal
 
 
 
@@ -80,6 +88,8 @@ try:
 
     nome=cadastro()
     print("\n{}, vamos começar? Escolha uma opção\n".format(nome))
+
+    #Estrutura de repetição a parti da qual o programa funcionará
     while(1):
         print("\n1. Financiamento de Veículos\n2. Financiamento de Casa\n")
         decisao=int(input("Digite a opção desejada.\n"))
@@ -89,22 +99,22 @@ try:
             print("O valor total a ser pago é:R$ {:.2f}".format(total))
 
         elif decisao==2:
-            total = casa()
+            total = imovel()
             print("O valor total a ser pago é:R$ {:.2f}".format(total))
         else:
             print("Digite o número corretamente!")
             break
 
+        while(True):
+            continua=str(input("\nDeseja continuar? Digite sim, se não quiser, digite nao.\n"))
 
-        continua=str(input("\nDeseja continuar? Digite sim, se não quiser, digite nao.\n"))
-
-        if(continua=="sim"):
-            continue
-        elif continua=="nao":
-            print("Até mais!")
-            break
-        else:
-            print("animal, digita corretamente!!!!")
+            if(continua=="sim"):
+                continue
+            elif continua=="nao":
+                print("\nAté mais!\n")
+                break
+            else:
+                print("\nDigite corretamente o dado acima.\n")
 
 except:
     print("Algo deu errado! Tente novamente")
